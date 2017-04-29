587c2c267351059a72678695264d8dba10174684
Anderson Dorow <andersondorow@gmail.com>
2017-04-29 20:34:48 +0200

    add: chapter 1, problems 11, 12, 14 and 15

--- src/main/scheme/exercises/01.15.scm
+++ src/main/scheme/exercises/01.15.scm
@@ -6,23 +6,21 @@
        (p (sine (/ angle 3.0)))))
 
 
-a.  How many times is the procedure p applied when (sine 12.15) is evaluated?
-
 ; a) p is called 5 times
 
-(sine 12.15)
-(p (sine (/ 12.15 3.0)))
-(p (sine 4.05))
-(p (p (sine (/ 4.05 3.0))))
-(p (p (sine 1.35)))
-(p (p (p (sine (/ 1.35 3.0)))))
-(p (p (p (sine 0.45))))
-(p (p (p (p (sine (/ 0.45 3.0))))))
-(p (p (p (p (sine 0.15)))))
-(p (p (p (p (p (sine (/ 0.15 3.0)))))))
-(p (p (p (p (p (sine 0.05))))))
-(p (p (p (p (p 0.05)))))
-...
+;(sine 12.15)
+;(p (sine (/ 12.15 3.0)))
+;(p (sine 4.05))
+;(p (p (sine (/ 4.05 3.0))))
+;(p (p (sine 1.35)))
+;(p (p (p (sine (/ 1.35 3.0)))))
+;(p (p (p (sine 0.45))))
+;(p (p (p (p (sine (/ 0.45 3.0))))))
+;(p (p (p (p (sine 0.15)))))
+;(p (p (p (p (p (sine (/ 0.15 3.0)))))))
+;(p (p (p (p (p (sine 0.05))))))
+;(p (p (p (p (p 0.05)))))
+;...
 
 ; b)
 ; the order of growth is O(log n); where n is the given angle.
